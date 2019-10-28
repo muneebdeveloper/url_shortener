d1e74a151c5431e3449b0b31389966542732a465
muneebdeveloper <muneebdeveloper@outlook.com>
2019-10-28 22:47:28 +0500

    config file edited

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `http://localhost:4000`;
+export const prodEndpoint = `https://url-shortner-backend-api.herokuapp.com`;
 export const wsEndpoint = `ws://localhost:4000`;
 export const perPage = 4;
